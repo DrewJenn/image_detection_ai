9b359d76e816c342ad86470f27381acc84e814ac
DrewJenn <drewj32@icloud.com>
2025-02-17 11:06:10 -0600

    Update label-img_launcher.py
    Now it opens the app with the specified input directory

--- label-img_launcher.py
+++ label-img_launcher.py
@@ -1,9 +1,14 @@
 import subprocess 
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'data')
-command = [('cd ' + os.path.join(file_path, 'launch_package')) + '\nmake qt5py3' + '\npython3 labelImg.py' ]
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+image_path = os.path.join(file_path, 'Transformed_Images')
+annotation_path = os.path.join(file_path, 'annotations')
+
+print("Please input the name of the AI we are annotating the data for: ")
+ai_project = input()
+
+command = [('cd ' + os.path.join(file_path, 'launch_package')) + '\nmake qt5py3' + '\npython3 labelImg.py' + ' ' + os.path.join(image_path, ai_project)]     
 try:
     result = subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print(f"Output of {' '.join(command)}:\n{result.stdout}")
